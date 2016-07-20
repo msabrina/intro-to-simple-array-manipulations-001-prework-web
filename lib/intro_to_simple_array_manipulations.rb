0aa33f5b15ad66cb47374513bc6867f22ea3d2b9
msabrina <msabrina88@gmail.com>
2016-07-20 15:26:07 -0500

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,15 +4,14 @@ def using_push (countries_in_western_africa, next_country)
   countries_in_western_africa.push(next_country)
 end
 
-def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
+def using_unshift (neighborhoods_in_northwest_brooklyn, new_neighborhood)
     neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
     @new_neighborhood = "Brooklyn Heights"
     neighborhoods_in_northwest_brooklyn.unshift(@new_neighborhood)
 end
 
-def using_pop(great_hits_of_the_nineties)
-    great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
-    @deleted_string = great_hits_of_the_nineties.pop 
+def using_pop (great_hits_of_the_nineties)
+   great_hits_of_the_nineties.pop(1) 
   end
 
 def pop_with_args (chars_in_game_of_thrones)
@@ -30,18 +29,36 @@ def shift_with_args (ice_cream_brands)
     @brands_removed = @ice_cream_brands.shift (2)
   end
 
-def using_concat(my_favorite_things, more_favs) 
+def using_concat (my_favorite_things, more_favs) 
   @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
     more_favs = ["mario kart", "flatiron school"]
     @all_my_favs = @my_favorite_things.concat(more_favs)
   end
 
- def using_insert(list_of_esoteric_programming_langguages, another_esoteric_language)
+ def using_insert (list_of_esoteric_programming_langguages, another_esoteric_language)
       @list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
       @another_esoteric_language = "Malbolge"
-      @new_array = using_insert(@list_of_esoteric_programming_languages, @another_esoteric_language)
+      @new_array = @list_of_esoteric_programming_languages.insert( 4, @another_esoteric_language)
 end
-  
+
+  def using_uniq (captain_planet_and_the_planeteers)
+    captain_planet_and_the_planeteers = ["Captain Planet", "Gaia", "Kwame", "Gi", "Linka", "Wheeler", "Gaia"]
+    captain_planet_and_the_planeteers.uniq
+  end
+
+  def using_flatten (private_colleges_in_newyork)
+      private_colleges_in_newyork = ["New York University", ["Manhattan School of Music ", "Columbia University"], "The Juilliard School", "Bard College", "Cooper Union"]
+      private_colleges_in_newyork.flatten(1)
+  end
+
+ def using_delete(array, string)
+  array.delete(string)
+end
+
+
+def using_delete_at(array, integer)
+  array.delete_at(integer)
+end
 
 
 
